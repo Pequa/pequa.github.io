58c01f956e9b617741c884bc64291a74c077d427
Pequa <Peter@Peters-MacBook-Pro.local>
2019-08-18 15:49:02 -0400

    Removing unused files, images, and fonts.

--- UAT/projects/controlpanel/Scripts/background.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $(window).scroll(function () {
-        var oVal;
-        oVal = $(window).scrollTop() / 100;
-        return $('.blur').css('opacity', oVal);
-    });
-}.call(this));
