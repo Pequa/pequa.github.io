eafd1c4ea51bd4ca4fe35692ffdebfcef522324c
Pequa <Peter@Peters-MacBook-Pro.local>
2019-08-20 08:32:53 -0400

    REDUX 3.0

--- UAT/home/Scripts/background.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $(window).scroll(function () {
-        var oVal;
-        oVal = $(window).scrollTop() / 440;
-        return $('.blur').css('opacity', oVal);
-    });
-}.call(this));
